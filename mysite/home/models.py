76740eeb483ef887c54795c0fa4298bd82313dad
kishoraditya <kishoradityasc@gmail.com>
2023-09-26 18:26:55 +0530

    modifying for user customization

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,13 +1,85 @@
 from django.db import models
+from django.shortcuts import render
+from wagtail.models import Page, Orderable
+from modelcluster.fields import ParentalKey
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel, PageChooserPanel, MultipleChooserPanel, InlinePanel
+from wagtail.images.blocks import ImageChooserBlock
+from streams import blocks
+from wagtail.contrib.routable_page.models import RoutablePageMixin, path
+class HomePage(RoutablePageMixin,Page):
+    """Home page model."""
+    max_count = 1
 
-from wagtail.models import Page
-from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
-
-
-class HomePage(Page):
-    body = RichTextField(blank=True)
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    content = StreamField([("cta", blocks.CTABlock()),], null=True, blank=True,use_json_field=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body'),
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        InlinePanel(
+            "images", 
+            label="Banner Images",
+        ),
+        FieldPanel("content"),
+        InlinePanel("carousel_images", 
+                    max_num=5, 
+                    min_num=1, 
+                    label="Image",
+            heading="Carousel Images",
+            ),
     ]
+
+    class Meta:
+
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
+      
+    @path('subscribe/')
+    def the_subscribe_page(self, request, *args, **kwargs):
+        return self.render(
+            request, 
+            context_overrides={
+                'context' : self.get_context(request, *args, **kwargs),
+            },
+            template="home/subscribe.html",
+        )
+
+
+
+class HomeImage(Orderable, models.Model):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='images')
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+   )
+    panels=[
+        FieldPanel("banner_image"),
+        FieldPanel("banner_cta"),
+    ]
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey(HomePage, related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [FieldPanel("carousel_image")]
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,24 +9,35 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 from blog import amp_urls as wagtail_amp_urls
 from django.conf.urls.i18n import i18n_patterns
+from wagtail.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
-    path("search/", search_views.search, name="search"),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('userauth.urls')),
+    path(r'^sitemap.xml$', sitemap),
 ]
+
+urlpatterns += i18n_patterns(
+    path('search/', search_views.search, name='search'),
+    path("", include(wagtail_urls)),
+    
+    prefix_default_language=False,
+)
 
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+    import debug_toolbar
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
@@ -47,8 +58,3 @@ urlpatterns += [
     re_path(r'', include(wagtail_urls)),
 ]
 
-urlpatterns += i18n_patterns(
-    path('search/', search_views.search, name='search'),
-    path("", include(wagtail_urls)),
-    prefix_default_language=False,
-)
