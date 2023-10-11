c0a7fd8a73daf070e0313f09bc5704551fa95caf
kishoraditya <kishoradityasc@gmail.com>
2023-10-11 13:05:38 +0530

    docker changes

--- mysite/mysite/settings/rm_local.py
+++ mysite/mysite/settings/rm_local.py
@@ -1,5 +1,5 @@
 #please rename the file from rm_local.py to local.py and then follow the following instructions
-
+import os
 #python3 manage.py shell
 #from django.core.management.utils import get_random_secret_key
 #get_random_secret_key()
@@ -7,12 +7,12 @@ SECRET_KEY = ""
 #replace with your db_name, username, and password and cut paste from base.py to local.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_name',
-        'USER': 'username',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('SQL_DATABASE','db_name'),
+        'USER': os.environ.get('SQL_USER','username'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD','password'),
+        'HOST': os.environ.get('SQL_HOST','127.0.0.1'),
+        'PORT': os.environ.get('SQL_PORT','5432'),
     }
 }
 
